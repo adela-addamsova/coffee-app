4200e32650af033d343fdf0f7676bf626363d369
adela-addamsova <a.addamsova@gmail.com>
2025-06-03 19:06:13 +0200

    feat: add new sections with info text, story, and menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import './App.css';
 import Header from './components/Header';
 import HeroSection from './home-page/HeroSection';
 import InfoBoxes from './home-page/InfoBoxes';
+import HeroTextSection from './home-page/HeroTextSection';
+import ScrollStory from './home-page/StoryScroll';
+import Menu from './home-page/MenuSection';
+
 
 
 
@@ -13,6 +16,9 @@ function App() {
       <Header />
       <HeroSection />
       <InfoBoxes />
+      <HeroTextSection />
+      <ScrollStory />
+      <Menu />
     </>
   );
 }
