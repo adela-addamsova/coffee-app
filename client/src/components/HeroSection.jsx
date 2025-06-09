40806877e079bea319957c30ffdf8174c5c60525
adela-addamsova <a.addamsova@gmail.com>
2025-06-09 15:05:13 +0200

    refactor: adjust hero section to be reusable

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,36 @@
+import './HeroSection.css';
+import MainButton from './MainButton';
+
+import './HeroSection.css';
+
+const HeroSection = ({
+  imgSrc,
+  heading,
+  subheading,
+  buttonText,
+  buttonHref,
+  buttonColor = 'white',
+  height,
+  className = '',
+  showText = true,
+}) => {
+  return (
+    <section className={`hero-section ${className}`} style={{ height }}>
+      <img className="hero-img" src={imgSrc} alt="" />
+      <div className="hero-img-overlay"></div>
+      
+      {showText && (
+        <div className="hero-text-overlay">
+          {heading && <h1 className="hero-heading">{heading}</h1>}
+          {subheading && <p className="hero-subheading">{subheading}</p>}
+          {buttonText && (
+            <MainButton text={buttonText} href={buttonHref} color={buttonColor} />
+          )}
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default HeroSection;
+
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,5 +1,8 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Header from '../components/Header';
-import HeroSection from '../home-page/HeroSection';
+import HeroSection from '../components/HeroSection';
+import HeroImg from '../assets/main-page/hero-1.jpg';
 import InfoBoxes from '../home-page/InfoBoxes';
 import HeroTextSection from '../home-page/HeroTextSection';
 import ScrollStory from '../home-page/StoryScroll';
@@ -8,11 +11,38 @@ import GallerySection from '../home-page/GallerySection';
 import ReservationSection from '../home-page/ReservationSection';
 import Footer from '../components/Footer';
 
+import './LandingPage.css';
+
 function LandingPage() {
+  // Scroll to section
+  const location = useLocation();
+
+  useEffect(() => {
+    const scrollToId = location.state?.scrollToId;
+
+    if (scrollToId) {
+      setTimeout(() => {
+        const el = document.getElementById(scrollToId);
+        if (el) {
+          el.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 100);
+    }
+  }, [location]);
+
   return (
     <>
       <Header />
-      <HeroSection />
+      <HeroSection
+        imgSrc={HeroImg}
+        heading="Morning Mist Coffee"
+        subheading="Fresh filter coffee, ready with the first light of morning."
+        buttonText="EXPLORE MORE"
+        buttonHref="#hero-text-section"
+        buttonColor="white"
+        height="100%"
+        className="home-hero"
+      />
       <InfoBoxes />
       <HeroTextSection />
       <ScrollStory />
