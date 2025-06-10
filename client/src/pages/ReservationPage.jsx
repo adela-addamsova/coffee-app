84f2d1597aeab1f4029990c7434b7be0d2dc73e9
adela-addamsova <a.addamsova@gmail.com>
2025-06-10 14:20:19 +0200

    style: change reservation page structure to place background image in the right corner

--- client/src/pages/ReservationPage.jsx
+++ client/src/pages/ReservationPage.jsx
@@ -2,8 +2,10 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import ReservationForm from '../reservation-page/ReservationForm';
 import HeroSection from '../components/HeroSection';
-import './ReservationPage.css';
+import './css/ReservationPage.css';
 import HeroImg from '../assets/reservation-page/reservation-hero.png';
+import BeansBackground from '../assets/reservation-page/beans-bck.png';
+import InfoBoxes from '../components/InfoBoxes'
 
 const ReservationPage = () => {
   return (
@@ -16,13 +18,17 @@ const ReservationPage = () => {
         buttonText={false}
         buttonHref={false}
       />
+      <div className='reservation-page-container'>
+      <InfoBoxes />
       <section className="main-content-reservation">
         <div className='reservation-form'>
           <ReservationForm />
         </div>
         <div className='reservation-img'>
+          <img src={BeansBackground} alt="Beans Background" />
         </div>
       </section>
+      </div>
       <Footer />
     </>
   );
