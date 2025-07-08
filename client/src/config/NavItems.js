d541634522e900bae8a9a9f8749167329642bd76
adela-addamsova <a.addamsova@gmail.com>
2025-07-08 17:26:38 +0200

    refactor: create reusable object for navigation items

---
+++ client/src/config/NavItems.js
@@ -0,0 +1,9 @@
+const navItems = [
+  { label: 'Menu', sectionId: 'menu-section' },
+  { label: 'Contact', sectionId: 'info-boxes' },
+  { label: 'Our Story', sectionId: 'story-section' },
+  { label: 'Reservation', to: '/reservation' },
+  { label: 'E-shop', sectionId: '' },
+];
+
+export default navItems;
