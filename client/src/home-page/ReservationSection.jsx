150d3a68f0c121e609987d7a7f31a42eb1bb1d0c
adela-addamsova <a.addamsova@gmail.com>
2025-07-09 16:29:03 +0200

    refactor: add shared nav link for button

--- client/src/home-page/ReservationSection.jsx
+++ client/src/home-page/ReservationSection.jsx
@@ -1,8 +1,10 @@
 import phoneBackground from '../assets/main-page/phone.png';
 import MainButton from '../components/MainButton';
-
+import navItems from '../config/NavItems';
 
 const ReservationSection = () => {
+    const reservationPageLink = navItems.find(item => item.label === 'Reservation');
+
     return (
         <section className="reservation-section" id='reservation-section'>
             <div className="reservation-content reservation-image">
@@ -15,7 +17,7 @@ const ReservationSection = () => {
                     social media or make a reservation
                     in our system.
                 </p>
-                <MainButton text="RESERVATION" to="/reservation" color="black" />
+                <MainButton text="RESERVATION" to={reservationPageLink.to} color="black" />
             </div>
         </section>
     );
