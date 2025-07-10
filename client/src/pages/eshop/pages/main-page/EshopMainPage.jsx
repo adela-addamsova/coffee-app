94ed41463165ddd71785fba39da339cffd8682ba
adela-addamsova <a.addamsova@gmail.com>
2025-07-10 17:45:53 +0200

    feat: create landing page for eshop and hero section including styling

---
+++ client/src/pages/eshop/pages/main-page/EshopMainPage.jsx
@@ -0,0 +1,26 @@
+import { react } from 'react';
+import Header from '../../../../components/Header';
+import Footer from '../../../../components/Footer';
+import HeroSection from '../../../../components/HeroSection';
+import HeroImg from '../../../../assets/e-shop/eshop-hero.jpg';
+import InlineMenu from '../../eshop-components/InlineMenu';
+
+const EshopLandingPage = () => {
+  return (
+    <>
+      <Header />
+      <div className='eshop-hero-section'>
+        <HeroSection
+          imgSrc={HeroImg}
+          heading="Freshly roasted coffee from our roastery"
+          subheading="Discover the diverse flavors of coffee from unique locations and regions"
+          className="eshop-hero"
+        />
+      </div>
+      < InlineMenu />
+      <Footer />
+    </>
+  );
+}
+
+export default EshopLandingPage;
