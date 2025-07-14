0d0d284a1d035a547a3cf345c6e2e75c2f0785d6
adela-addamsova <a.addamsova@gmail.com>
2025-07-14 21:02:44 +0200

    fix: change order if nav items

--- client/src/config/NavItems.js
+++ client/src/config/NavItems.js
@@ -1,15 +1,15 @@
 export const navItems = [
-  { label: 'Menu', sectionId: 'menu-section' },
+  { label: 'Reservation', to: '/reservation' },
+  { label: 'E-shop', to: '/e-shop' },
   { label: 'Contact', sectionId: 'info-boxes' },
   { label: 'Our Story', sectionId: 'story-section' },
-  { label: 'Reservation', to: '/reservation' },
-  { label: 'E-shop', to: '/e-shop' }
+  { label: 'Menu', sectionId: 'menu-section' },
 ];
 
 export const eshopNavItems = [
-  { label: 'All Products', to: '/e-shop/products'},
-  { label: 'Light Roasted', to: '/e-shop/products/light'},
-  { label: 'Dark Roasted', to: '/e-shop/products/dark'},
-  { label: 'Decaf', to: '/e-shop/products/decaf'},
+  { label: 'All Products', to: '/e-shop/products' },
+  { label: 'Light Roasted', to: '/e-shop/products/light' },
+  { label: 'Dark Roasted', to: '/e-shop/products/dark' },
+  { label: 'Decaf', to: '/e-shop/products/decaf' },
 ];
 
