36efcd412e1959c702b2a1b269b07fc3018901e0
adela-addamsova <a.addamsova@gmail.com>
2025-07-14 15:49:21 +0200

    feat: import link for product categories

--- client/src/pages/eshop/pages/main-page/ProductCategories.jsx
+++ client/src/pages/eshop/pages/main-page/ProductCategories.jsx
@@ -2,43 +2,48 @@ import { Link } from 'react-router-dom';
 import LightRoasted from '../../../../assets/e-shop/light-roasted.png';
 import DarkRoasted from '../../../../assets/e-shop/dark-roasted.png';
 import Decaf from '../../../../assets/e-shop/decaf.png';
+import { eshopNavItems} from '../../../../config/NavItems';
+
+const navLight = eshopNavItems.find(item => item.label === 'Light Roasted');
+const navDark = eshopNavItems.find(item => item.label === 'Dark Roasted');
+const navDecaf = eshopNavItems.find(item => item.label === 'Decaf');
 
 const categories = [
   {
     name: 'LIGHT ROASTED',
     image: LightRoasted,
-    link: '#',
+    link: navLight.to,
   },
   {
     name: 'DARK ROASTED',
     image: DarkRoasted,
-    link: '#',
+    link: navDark.to,
   },
   {
     name: 'DECAF',
     image: Decaf,
-    link: '#',
+    link: navDecaf.to,
   },
 ];
 
 export default function ProductCategories() {
   return (
     <div className="product-categories-section">
-  {categories.map((item) => (
-    <Link key={item.name} to={item.link} className="product-category group">
-      <div className="img-box">
-        <img
-          src={item.image}
-          alt={item.name}
-          className="category-img"
-        />
-        <div className="img-overlay"></div>
-      </div>
-      <div className="category-name">
-        {item.name}
-      </div>
-    </Link>
-  ))}
-</div>
+      {categories.map((item) => (
+        <Link key={item.name} to={item.link} className="product-category group">
+          <div className="img-box">
+            <img
+              src={item.image}
+              alt={item.name}
+              className="category-img"
+            />
+            <div className="img-overlay"></div>
+          </div>
+          <div className="category-name">
+            {item.name}
+          </div>
+        </Link>
+      ))}
+    </div>
   );
 }
