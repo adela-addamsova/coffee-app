d8eeb240ded447786135012f976b817397ff8921
adela-addamsova <a.addamsova@gmail.com>
2025-07-31 11:16:17 +0200

    refactor: use routes with production database

--- server/server.ts
+++ server/server.ts
@@ -1,27 +1,38 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import db from "@db/coffee-app-db";
+import reservationRouter from "@routes/reservations.routes";
+import productRoutes from "@routes/products.routes";
+import newsletterRouter from "@routes/subscribtion.routes";
+import initializeDatabase from "@db/init-db";
 
-import initializeDatabase from './db/init-db';
-initializeDatabase();
-
-import reservationRoutes from './routes/reservations.routes';
-import productRoutes from './routes/products.routes';
-import subscriptionRoutes from './routes/subscribtion.routes';
+initializeDatabase(db);
 
 const app = express();
 
 dotenv.config();
 
-app.use(cors({
-  origin: process.env.CLIENT_ORIGIN,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN,
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
-app.use('/api/reservations', reservationRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/subscribe', subscriptionRoutes);
+app.use("/api/reservations", reservationRouter(db));
+app.use("/api/products", productRoutes(db));
+app.use("/api/subscribe", newsletterRouter(db));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+function startServer() {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT);
+}
+
+if (require.main === module) {
+  startServer();
+}
+
+export default app;
