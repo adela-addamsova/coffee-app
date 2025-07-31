2d79189f063f616b8725e1804296db1c745ba020
adela-addamsova <a.addamsova@gmail.com>
2025-07-31 11:14:05 +0200

    refactor: use db instance instead of hardcoded db in routes functions

--- server/routes/subscribtion.routes.ts
+++ server/routes/subscribtion.routes.ts
@@ -4,42 +4,49 @@ import {
   isEmailSubscribed,
 } from "@db/newsletter-db";
 import { newsletterSchema } from "@shared/NewsletterValidationSchema";
+import type { Database as DBType } from "better-sqlite3";
 
-const router = Router();
+export default function newsletterRouter(dbInstance: DBType) {
+  const router = Router();
 
-/**
- * POST /api/subscribe
- * Validates input with Zod and inserts a subscriber
- *
- * @param req - Express Request object containing email in the body
- * @param res - Express Response object used to send the result
- * @returns JSON response with success message or error
- */
-router.post("/", (req: Request, res: Response) => {
-  const parseResult = newsletterSchema.safeParse(req.body);
+  /**
+   * POST /api/subscribe
+   * Validates input with Zod and inserts a subscriber
+   *
+   * @param req - Express Request object containing email in the body
+   * @param res - Express Response object used to send the result
+   * @returns JSON response with success message or error
+   */
+  router.post("/", (req: Request, res: Response) => {
+    const parseResult = newsletterSchema.safeParse(req.body);
 
-  if (!parseResult.success) {
-    return res.status(400).json({ error: parseResult.error.errors[0].message });
-  }
-
-  const { email } = parseResult.data;
-  const ip_address = req.ip;
-
-  try {
-    if (isEmailSubscribed(email)) {
+    if (!parseResult.success) {
       return res
-        .status(409)
-        .json({ error: "This email is already subscribed" });
+        .status(400)
+        .json({ error: parseResult.error.errors[0].message });
     }
 
-    const result = insertNewsletterSubscriber(email, ip_address);
-    return res
-      .status(201)
-      .json({ message: "Subscription successful", id: result.lastInsertRowid });
-  } catch (err) {
-    console.error("Insert error:", err);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
+    const { email } = parseResult.data;
+    const ip_address = req.ip;
 
-export default router;
+    try {
+      if (isEmailSubscribed(email, dbInstance)) {
+        return res
+          .status(409)
+          .json({ error: "This email is already subscribed" });
+      }
+
+      const result = insertNewsletterSubscriber(email, ip_address, dbInstance);
+      return res
+        .status(201)
+        .json({
+          message: "Subscription successful",
+          id: result.lastInsertRowid,
+        });
+    } catch (_err) {
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
+
+  return router;
+}
