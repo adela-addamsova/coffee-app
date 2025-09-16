24e596b2d2e91078ec386e637e5333eddb5e36a1
adela-addamsova <a.addamsova@gmail.com>
2025-09-16 13:38:33 +0200

    feat: add link to shopping cart in eshopNavItems

--- client/src/config/NavItems.ts
+++ client/src/config/NavItems.ts
@@ -5,16 +5,17 @@ export interface NavItem {
 }
 
 export const navItems: NavItem[] = [
-  { label: 'Reservation', to: '/reservation' },
-  { label: 'E-shop', to: '/e-shop' },
-  { label: 'Contact', sectionId: 'info-boxes' },
-  { label: 'Our Story', sectionId: 'story-section' },
-  { label: 'Menu', sectionId: 'menu-section' },
+  { label: "Reservation", to: "/reservation" },
+  { label: "E-shop", to: "/e-shop" },
+  { label: "Contact", sectionId: "info-boxes" },
+  { label: "Our Story", sectionId: "story-section" },
+  { label: "Menu", sectionId: "menu-section" },
 ];
 
 export const eshopNavItems: NavItem[] = [
-  { label: 'All Products', to: '/e-shop/products' },
-  { label: 'Light Roasted', to: '/e-shop/products/light' },
-  { label: 'Dark Roasted', to: '/e-shop/products/dark' },
-  { label: 'Decaf', to: '/e-shop/products/decaf' },
+  { label: "All Products", to: "/e-shop/products" },
+  { label: "Light Roasted", to: "/e-shop/products/light" },
+  { label: "Dark Roasted", to: "/e-shop/products/dark" },
+  { label: "Decaf", to: "/e-shop/products/decaf" },
+  { label: "Shopping Cart", to: "/e-shop/cart" },
 ];
