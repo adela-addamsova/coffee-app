29f71836cdb28f654b6f778017555435772cc02e
adela-addamsova <a.addamsova@gmail.com>
2025-09-18 13:56:36 +0200

    refactor: make CartLayout reusable template and place cart data into CartStepOne

--- client/src/pages/eshop/pages/cart/CartLayout.tsx
+++ client/src/pages/eshop/pages/cart/CartLayout.tsx
@@ -1,29 +1,27 @@
-import { JSX } from "react";
+import { JSX, ReactNode } from "react";
 import HeroSection from "@components/HeroSection";
 import HeroImg from "@assets/e-shop/category-hero.jpg";
 import Newsletter from "@eshop-components/Newsletter";
 import cartIcon from "@assets/e-shop/cart/shopping-cart.svg";
 import delivery from "@assets/e-shop/cart/delivery-gray.svg";
+import deliveryActive from "@assets/e-shop/cart/delivery.svg";
 import payment from "@assets/e-shop/cart/payment-gray.svg";
-import deleteIcon from "@assets/e-shop/cart/delete.svg";
-import { useCart } from "@eshop/pages/cart/CartContext";
-import ProductCounter from "@eshop-components/ProductCounter";
-import MainButton from "@/components/MainButton";
-import { navItems } from "@config/NavItems";
+import paymentActive from "@assets/e-shop/cart/payment.svg";
 
-export default function CartLayout(): JSX.Element {
-  const { cart, categoryLabels, updateQuantity, removeFromCart } = useCart();
+type CartLayoutProps = {
+  step: "cart" | "delivery" | "payment";
+  children: ReactNode;
+};
 
-  const totalPrice = cart.reduce(
-    (sum, item) => sum + item.price * item.quantity,
-    0,
-  );
-
-  const DPH_RATE = 0.21;
-
-  const totalPriceWithoutDPH = totalPrice / (1 + DPH_RATE);
-
-  const eshopUrl = navItems.find((item) => item.label === "E-shop")?.to;
+export default function CartLayout({
+  step,
+  children,
+}: CartLayoutProps): JSX.Element {
+  const steps = [
+    { label: "cart", icon: cartIcon, activeIcon: cartIcon },
+    { label: "delivery", icon: delivery, activeIcon: deliveryActive },
+    { label: "payment", icon: payment, activeIcon: paymentActive },
+  ];
 
   return (
     <div className="cart-page">
@@ -31,83 +29,34 @@ export default function CartLayout(): JSX.Element {
         <div className="product-page-hero">
           <HeroSection imgSrc={HeroImg} />
         </div>
+
         <div className="shopping-cart-container">
-          <div className="cart-menu">
-            <div>
-              <div className="icon-item">
-                <img
-                  src={cartIcon}
-                  alt="Shopping Cart"
-                  className="cart-menu-icon active"
-                />
-              </div>
-            </div>
-            <div className="icon-item">
-              <img src={delivery} alt="Delivery" className="cart-menu-icon" />
-            </div>
-            <div className="icon-item">
-              <img src={payment} alt="Payment" className="cart-menu-icon" />
-            </div>
-          </div>
-        </div>
+          <div className="cart-menu-container">
+            <div className="cart-menu-line"></div>
+            <div className="cart-menu-line-progress" data-step={step}></div>
 
-        <div className="shopping-cart-content">
-          {cart.length === 0 ? (
-            <h2 className="empty-cart">Cart is empty</h2>
-          ) : (
-            <div className="cart-summary">
-              {cart.map((item) => (
-                <div key={item.id} className="cart-item">
-                  <div className="flex flex-row items-center">
+            <div className="cart-menu">
+              {steps.map((s, index) => {
+                const isActive =
+                  ["cart", "delivery", "payment"].indexOf(step) >= index;
+                return (
+                  <div key={s.label} className="icon-item">
                     <img
-                      src={item.image_url}
-                      alt={item.title}
-                      className="product-img"
+                      src={isActive ? s.activeIcon : s.icon}
+                      alt={s.label}
+                      className={`cart-menu-icon ${isActive ? "active" : ""}`}
                     />
-                    <h4 className="product-desc">
-                      {item.title} – {categoryLabels[item.category]}{" "}
-                      {item.weight}
-                    </h4>
                   </div>
-                  <div className="flex flex-row items-center gap-8">
-                    <ProductCounter
-                      min={1}
-                      max={item.stock}
-                      value={item.quantity}
-                      onChange={(newQty) => updateQuantity(item.id, newQty)}
-                    />
-                    <h4 className="product-desc">
-                      ${(item.price * item.quantity).toFixed(2)}
-                    </h4>
-                    <div
-                      className="product-info-button"
-                      onClick={() => removeFromCart(item.id)}
-                    >
-                      <img src={deleteIcon} alt={item.title} />
-                    </div>
-                  </div>
-                </div>
-              ))}
+                );
+              })}
             </div>
-          )}
+          </div>
         </div>
 
-        <div className="total-price">
-          <h3>Order Summary</h3>
-          <div className="total-inner">
-            <h4>Total</h4>
-            <h4>${totalPrice.toFixed(2)}</h4>
-          </div>
-          <div className="total-inner">
-            <h5>Price without DPH</h5>
-            <h5>${totalPriceWithoutDPH.toFixed(2)}</h5>
-          </div>
-          <div className="total-buttons">
-            <MainButton text="BACK TO ESHOP" to={eshopUrl} color="black" />
-            <MainButton text="NEXT" to={eshopUrl} color="white" />
-          </div>
-        </div>
+        {/* Variable inner content */}
+        {children}
       </section>
+
       <Newsletter />
     </div>
   );
