1a79f930fbbc3db73ea5276c3a75abb46f3c138c
adela-addamsova <a.addamsova@gmail.com>
2025-10-15 12:28:44 +0200

    refactor: add divs, change class names

--- client/src/pages/eshop/pages/cart/MockCartPayment.tsx
+++ client/src/pages/eshop/pages/cart/MockCartPayment.tsx
@@ -38,58 +38,67 @@ export default function MockPaymentForm({
 
       <div className="payment-form-data">
         {/* Card number */}
-        <div className="radio-form-group">
-          <input
-            placeholder="1234 1234 1234 1234"
-            maxLength={19}
-            inputMode="numeric"
-            pattern="\d*"
-            value={cardData.cardNumber}
-            onInput={(e) => {
-              let v = e.currentTarget.value.replace(/\D/g, "");
-              v = v.replace(/(\d{4})(?=\d)/g, "$1 ");
-              handleChange("cardNumber", v);
-              e.currentTarget.value = v;
-            }}
-          />
-          {errors.cardNumber && (
-            <p className="field-error-message">{errors.cardNumber.message}</p>
-          )}
-        </div>
-
-        {/* Expiry + CVV */}
-        <div className="cart-info-50">
-          <div className="radio-form-group">
+        <div>
+          <div className="payment-form-group">
             <input
-              placeholder="MM/YY"
-              maxLength={5}
+              placeholder="1234 1234 1234 1234"
+              maxLength={19}
               inputMode="numeric"
-              value={cardData.expiry}
+              pattern="\d*"
+              value={cardData.cardNumber}
               onInput={(e) => {
                 let v = e.currentTarget.value.replace(/\D/g, "");
-                if (v.length >= 3) v = v.slice(0, 2) + "/" + v.slice(2, 4);
-                handleChange("expiry", v);
+                v = v.replace(/(\d{4})(?=\d)/g, "$1 ");
+                handleChange("cardNumber", v);
                 e.currentTarget.value = v;
               }}
             />
+          </div>
+        </div>
+
+        {errors.cardNumber && (
+          <p className="field-error-message mb-4">
+            {errors.cardNumber.message}
+          </p>
+        )}
+
+        {/* Expiry + CVV */}
+        <div className="cart-info-50">
+          <div className="cart-info-50-inner">
+            <div className="payment-form-group">
+              <input
+                placeholder="MM/YY"
+                maxLength={5}
+                inputMode="numeric"
+                value={cardData.expiry}
+                onInput={(e) => {
+                  let v = e.currentTarget.value.replace(/\D/g, "");
+                  if (v.length >= 3) v = v.slice(0, 2) + "/" + v.slice(2, 4);
+                  handleChange("expiry", v);
+                  e.currentTarget.value = v;
+                }}
+              />
+            </div>
             {errors.expiry && (
               <p className="field-error-message">{errors.expiry.message}</p>
             )}
           </div>
 
-          <div className="radio-form-group">
-            <input
-              placeholder="CVV"
-              maxLength={3}
-              inputMode="numeric"
-              pattern="\d*"
-              value={cardData.cvv}
-              onInput={(e) => {
-                const v = e.currentTarget.value.replace(/\D/g, "");
-                handleChange("cvv", v);
-                e.currentTarget.value = v;
-              }}
-            />
+          <div className="cart-info-50-inner">
+            <div className="payment-form-group">
+              <input
+                placeholder="CVV"
+                maxLength={3}
+                inputMode="numeric"
+                pattern="\d*"
+                value={cardData.cvv}
+                onInput={(e) => {
+                  const v = e.currentTarget.value.replace(/\D/g, "");
+                  handleChange("cvv", v);
+                  e.currentTarget.value = v;
+                }}
+              />
+            </div>
             {errors.cvv && (
               <p className="field-error-message">{errors.cvv.message}</p>
             )}
