821a2d92271a4127cf3ef291a45c43f294ac79f3
adela-addamsova <a.addamsova@gmail.com>
2025-10-24 19:18:48 +0200

    chore: set aditional  config for tests

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.test" });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -25,6 +27,9 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
+  },
+  define: {
+    "import.meta.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL),
   },
   test: {
     globals: true,
